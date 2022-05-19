9f807e1b8c4b81fe56082698a0bbed9d96d057f6
“BeneditoRodrigo <rodrigodacunha7@gmail.com>
2022-05-19 13:51:38 -0300

    Funcao Match

---
+++ Match.java
@@ -0,0 +1,31 @@
+package modulostreams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Match {
+
+	public static void main(String[] args) {
+		
+		Aluno a1 = new Aluno("Ana", 7.2);
+		Aluno a2 = new Aluno("Luna", 7.1);
+		Aluno a3 = new Aluno("Gui", 8.2);
+		Aluno a4 = new Aluno("Gabi", 10);
+
+		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4);
+
+		Predicate<Aluno> aprovado = a -> a.nota >= 7;
+		Predicate<Aluno> reprovado = aprovado.negate();
+		
+		//saber se todos forem aprovados
+		System.out.println(alunos.stream().allMatch(aprovado));
+		//saber se alguém foi aprovado
+		System.out.println(alunos.stream().anyMatch(aprovado));
+		//nenhum deu aprovado?
+		System.out.println(alunos.stream().noneMatch(aprovado));
+		//nenhum deu reprovado?
+		System.out.println(alunos.stream().noneMatch(reprovado));
+	}
+
+}
